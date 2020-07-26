25fe59ed667bc74f39e20f8c8a53f333ec260cf4
Kaelin Laundry <wasabifan@outlook.com>
2020-07-25 22:11:57 -0700

    Improved basic evaluator test coverage

--- src/evaluator/engine.rs
+++ src/evaluator/engine.rs
@@ -152,6 +152,43 @@ mod tests {
     }
 
     #[test]
+    fn test_multi_print() -> Result<(), EvaluationError> {
+        let env = Rc::new(TestEnvironment::default());
+        let mut engine = ExecutionEngine::new(env.clone());
+
+        let record = Record::new(
+            String::from("foo bar"),
+            vec![String::from("foo"), String::from("bar")],
+        );
+        engine.execute_statement(
+            &record,
+            &Statement::Command(
+                BuiltinCommand::Print,
+                vec![
+                    Expression::UnaryOperation(
+                        UnOp::FieldReference,
+                        Box::new(Expression::NumericLiteral(0.)),
+                    ),
+                    Expression::UnaryOperation(
+                        UnOp::FieldReference,
+                        Box::new(Expression::NumericLiteral(1.)),
+                    ),
+                    Expression::UnaryOperation(
+                        UnOp::FieldReference,
+                        Box::new(Expression::NumericLiteral(2.)),
+                    ),
+                ],
+            ),
+        )?;
+
+        assert_eq!(
+            env.get_printed_lines(),
+            vec![String::from("foo bar foo bar\n")]
+        );
+        Ok(())
+    }
+
+    #[test]
     fn test_field_reference_basic() -> Result<(), EvaluationError> {
         let env = Rc::new(TestEnvironment::default());
         let mut engine = ExecutionEngine::new(env.clone());
--- src/evaluator/mod.rs
+++ src/evaluator/mod.rs
@@ -139,7 +139,9 @@ impl ProgramEvaluator {
 #[cfg(test)]
 mod tests {
     use super::{test_utils::TestEnvironment, EvaluationError, ProgramEvaluator, VariableValue};
-    use crate::parser::ast::{Action, Ast, Pattern, Rule};
+    use crate::parser::ast::{
+        Action, Ast, BuiltinCommand, Expression, Pattern, Rule, Statement, UnOp,
+    };
     use std::rc::Rc;
 
     #[test]
@@ -188,5 +190,54 @@ mod tests {
                 "i_dont_exist"
             )))
         );
+    }
+
+    #[test]
+    fn test_begin_present_action() -> Result<(), EvaluationError> {
+        let program = Ast {
+            rules: vec![Rule {
+                pattern: Pattern::Begin,
+                action: Action::Present(vec![Statement::Command(
+                    BuiltinCommand::Print,
+                    vec![Expression::NumericLiteral(5.)],
+                )]),
+            }],
+        };
+
+        let env = Rc::new(TestEnvironment::default());
+        let evaluator = ProgramEvaluator::new(program, env.clone());
+        evaluator.begin()?;
+
+        assert_eq!(env.get_printed_lines(), vec![String::from("5\n")]);
+        Ok(())
+    }
+
+    #[test]
+    fn test_begin_record_value() -> Result<(), EvaluationError> {
+        let program = Ast {
+            rules: vec![Rule {
+                pattern: Pattern::Begin,
+                action: Action::Present(vec![Statement::Command(
+                    BuiltinCommand::Print,
+                    vec![
+                        Expression::UnaryOperation(
+                            UnOp::FieldReference,
+                            Box::new(Expression::NumericLiteral(0.)),
+                        ),
+                        Expression::UnaryOperation(
+                            UnOp::FieldReference,
+                            Box::new(Expression::NumericLiteral(1.)),
+                        ),
+                    ],
+                )]),
+            }],
+        };
+
+        let env = Rc::new(TestEnvironment::default());
+        let evaluator = ProgramEvaluator::new(program, env.clone());
+        evaluator.begin()?;
+
+        assert_eq!(env.get_printed_lines(), vec![String::from(" \n")]);
+        Ok(())
     }
 }
