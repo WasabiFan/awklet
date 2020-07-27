e47b8c7f037ad4dde84ce39315aa373ed8b23d9f
Kaelin Laundry <wasabifan@outlook.com>
2020-07-26 17:44:25 -0700

    More thorough comparison test coverage

--- src/evaluator/tests/engine/comparison.rs
+++ src/evaluator/tests/engine/comparison.rs
@@ -120,3 +120,254 @@ fn compare_less_equal_given_greater_numeric() -> Result<(), EvaluationError> {
     assert_eq!(value, VariableValue::Numeric(0.));
     Ok(())
 }
+
+
+#[test]
+fn compare_equal_given_less_numeric() -> Result<(), EvaluationError> {
+    let env = Rc::new(TestEnvironment::default());
+    let mut engine = ExecutionEngine::new(env.clone());
+
+    let mut record = Record::default();
+    let value = engine.evaluate_expression(
+        &mut record,
+        &Expression::BinaryOperation(
+            BinOp::CompareEquals,
+            Box::new(Expression::NumericLiteral(19.)),
+            Box::new(Expression::NumericLiteral(19.8)),
+        ),
+    )?;
+
+    assert_eq!(value, VariableValue::Numeric(0.));
+    Ok(())
+}
+
+#[test]
+fn compare_equal_given_equal_numeric() -> Result<(), EvaluationError> {
+    let env = Rc::new(TestEnvironment::default());
+    let mut engine = ExecutionEngine::new(env.clone());
+
+    let mut record = Record::default();
+    let value = engine.evaluate_expression(
+        &mut record,
+        &Expression::BinaryOperation(
+            BinOp::CompareEquals,
+            Box::new(Expression::NumericLiteral(19.8)),
+            Box::new(Expression::NumericLiteral(19.8)),
+        ),
+    )?;
+
+    assert_eq!(value, VariableValue::Numeric(1.));
+    Ok(())
+}
+
+#[test]
+fn compare_equal_given_greater_numeric() -> Result<(), EvaluationError> {
+    let env = Rc::new(TestEnvironment::default());
+    let mut engine = ExecutionEngine::new(env.clone());
+
+    let mut record = Record::default();
+    let value = engine.evaluate_expression(
+        &mut record,
+        &Expression::BinaryOperation(
+            BinOp::CompareEquals,
+            Box::new(Expression::NumericLiteral(19.9)),
+            Box::new(Expression::NumericLiteral(19.8)),
+        ),
+    )?;
+
+    assert_eq!(value, VariableValue::Numeric(0.));
+    Ok(())
+}
+
+
+#[test]
+fn compare_not_equal_given_less_numeric() -> Result<(), EvaluationError> {
+    let env = Rc::new(TestEnvironment::default());
+    let mut engine = ExecutionEngine::new(env.clone());
+
+    let mut record = Record::default();
+    let value = engine.evaluate_expression(
+        &mut record,
+        &Expression::BinaryOperation(
+            BinOp::CompareNotEquals,
+            Box::new(Expression::NumericLiteral(19.)),
+            Box::new(Expression::NumericLiteral(19.8)),
+        ),
+    )?;
+
+    assert_eq!(value, VariableValue::Numeric(1.));
+    Ok(())
+}
+
+#[test]
+fn compare_not_equal_given_equal_numeric() -> Result<(), EvaluationError> {
+    let env = Rc::new(TestEnvironment::default());
+    let mut engine = ExecutionEngine::new(env.clone());
+
+    let mut record = Record::default();
+    let value = engine.evaluate_expression(
+        &mut record,
+        &Expression::BinaryOperation(
+            BinOp::CompareNotEquals,
+            Box::new(Expression::NumericLiteral(19.8)),
+            Box::new(Expression::NumericLiteral(19.8)),
+        ),
+    )?;
+
+    assert_eq!(value, VariableValue::Numeric(0.));
+    Ok(())
+}
+
+#[test]
+fn compare_not_equal_given_greater_numeric() -> Result<(), EvaluationError> {
+    let env = Rc::new(TestEnvironment::default());
+    let mut engine = ExecutionEngine::new(env.clone());
+
+    let mut record = Record::default();
+    let value = engine.evaluate_expression(
+        &mut record,
+        &Expression::BinaryOperation(
+            BinOp::CompareNotEquals,
+            Box::new(Expression::NumericLiteral(19.9)),
+            Box::new(Expression::NumericLiteral(19.8)),
+        ),
+    )?;
+
+    assert_eq!(value, VariableValue::Numeric(1.));
+    Ok(())
+}
+
+
+#[test]
+fn compare_greater_equal_given_less_numeric() -> Result<(), EvaluationError> {
+    let env = Rc::new(TestEnvironment::default());
+    let mut engine = ExecutionEngine::new(env.clone());
+
+    let mut record = Record::default();
+    let value = engine.evaluate_expression(
+        &mut record,
+        &Expression::BinaryOperation(
+            BinOp::CompareGreaterEquals,
+            Box::new(Expression::NumericLiteral(19.)),
+            Box::new(Expression::NumericLiteral(19.8)),
+        ),
+    )?;
+
+    assert_eq!(value, VariableValue::Numeric(0.));
+    Ok(())
+}
+
+#[test]
+fn compare_greater_equal_given_equal_numeric() -> Result<(), EvaluationError> {
+    let env = Rc::new(TestEnvironment::default());
+    let mut engine = ExecutionEngine::new(env.clone());
+
+    let mut record = Record::default();
+    let value = engine.evaluate_expression(
+        &mut record,
+        &Expression::BinaryOperation(
+            BinOp::CompareGreaterEquals,
+            Box::new(Expression::NumericLiteral(19.8)),
+            Box::new(Expression::NumericLiteral(19.8)),
+        ),
+    )?;
+
+    assert_eq!(value, VariableValue::Numeric(1.));
+    Ok(())
+}
+
+#[test]
+fn compare_greater_equal_given_greater_numeric() -> Result<(), EvaluationError> {
+    let env = Rc::new(TestEnvironment::default());
+    let mut engine = ExecutionEngine::new(env.clone());
+
+    let mut record = Record::default();
+    let value = engine.evaluate_expression(
+        &mut record,
+        &Expression::BinaryOperation(
+            BinOp::CompareGreaterEquals,
+            Box::new(Expression::NumericLiteral(19.9)),
+            Box::new(Expression::NumericLiteral(19.8)),
+        ),
+    )?;
+
+    assert_eq!(value, VariableValue::Numeric(1.));
+    Ok(())
+}
+
+
+#[test]
+fn compare_greater_given_less_numeric() -> Result<(), EvaluationError> {
+    let env = Rc::new(TestEnvironment::default());
+    let mut engine = ExecutionEngine::new(env.clone());
+
+    let mut record = Record::default();
+    let value = engine.evaluate_expression(
+        &mut record,
+        &Expression::BinaryOperation(
+            BinOp::CompareGreater,
+            Box::new(Expression::NumericLiteral(19.)),
+            Box::new(Expression::NumericLiteral(19.8)),
+        ),
+    )?;
+
+    assert_eq!(value, VariableValue::Numeric(0.));
+    Ok(())
+}
+
+#[test]
+fn compare_greater_given_equal_numeric() -> Result<(), EvaluationError> {
+    let env = Rc::new(TestEnvironment::default());
+    let mut engine = ExecutionEngine::new(env.clone());
+
+    let mut record = Record::default();
+    let value = engine.evaluate_expression(
+        &mut record,
+        &Expression::BinaryOperation(
+            BinOp::CompareGreater,
+            Box::new(Expression::NumericLiteral(19.8)),
+            Box::new(Expression::NumericLiteral(19.8)),
+        ),
+    )?;
+
+    assert_eq!(value, VariableValue::Numeric(0.));
+    Ok(())
+}
+
+#[test]
+fn compare_greater_given_greater_numeric() -> Result<(), EvaluationError> {
+    let env = Rc::new(TestEnvironment::default());
+    let mut engine = ExecutionEngine::new(env.clone());
+
+    let mut record = Record::default();
+    let value = engine.evaluate_expression(
+        &mut record,
+        &Expression::BinaryOperation(
+            BinOp::CompareGreater,
+            Box::new(Expression::NumericLiteral(19.9)),
+            Box::new(Expression::NumericLiteral(19.8)),
+        ),
+    )?;
+
+    assert_eq!(value, VariableValue::Numeric(1.));
+    Ok(())
+}
+
+#[test]
+fn compare_strings() -> Result<(), EvaluationError> {
+    let env = Rc::new(TestEnvironment::default());
+    let mut engine = ExecutionEngine::new(env.clone());
+
+    let mut record = Record::default();
+    let value = engine.evaluate_expression(
+        &mut record,
+        &Expression::BinaryOperation(
+            BinOp::CompareGreaterEquals,
+            Box::new(Expression::StringLiteral(String::from("abc"))),
+            Box::new(Expression::StringLiteral(String::from("xyz"))),
+        ),
+    )?;
+
+    assert_eq!(value, VariableValue::Numeric(0.));
+    Ok(())
+}
